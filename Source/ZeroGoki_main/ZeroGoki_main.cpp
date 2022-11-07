8df97d86480c9985083fc1404228002e76864eee
Nasyrov Takhir <tahanasyrov@gmail.com>
2022-11-07 18:37:57 +0100

    Delete Source directory

--- Source/ZeroGoki_main/ZeroGoki_main.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ZeroGoki_main.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZeroGoki_main, "ZeroGoki_main" );
- 
